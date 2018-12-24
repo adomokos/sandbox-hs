3f5f33715a271498bfa81eae82f5bdfcd0bb243d
Attila Domokos <adomokos@gmail.com>
2018-12-24 15:08:09 -0600

    A bit of Monad Transformers

---
+++ test/MonadTransformers/IntroSpec.hs
@@ -0,0 +1,38 @@
+module MonadTransformers.Intro where
+
+import Test.Hspec
+
+import Control.Monad
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Class
+
+run :: String -> IO String
+run pwd = do
+  password <- runMaybeT $ getPassword pwd
+  case password of
+    Just p  -> pure "valid password!"
+    Nothing -> pure "invalid password!"
+
+isValid :: String -> Bool
+isValid = (>= 10) . length
+
+getPwdLine :: String -> IO String
+getPwdLine = pure
+
+getPassword :: String -> MaybeT IO String
+getPassword pwd = do
+  password <- lift $ getPwdLine pwd
+  guard (isValid password)
+  pure pwd
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "Monad Transformers" $ do
+    it "can transform" $ do
+      result <- run "some"
+      result `shouldBe` "invalid password!"
+      resultGood <- run "myLongPa$$word"
+      resultGood `shouldBe` "valid password!"
