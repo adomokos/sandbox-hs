26e7ab9ac1d8dc78bf8755d8034ce1ff536345c4
Attila Domokos <adomokos@gmail.com>
2019-02-07 22:10:23 -0600

    Add European log file format parsing

--- test/Parsing/AttoparsecSpec.hs
+++ test/Parsing/AttoparsecSpec.hs
@@ -12,7 +12,7 @@ import qualified Data.Time as DT
 
 -- | Types for the log file content
 data Product = Mouse | Keyboard | Monitor | Speakers
-  deriving (Eq, Show)
+  deriving (Eq, Show, Enum)
 
 data LogEntry =
   LogEntry { entryTime :: DT.LocalTime
@@ -93,6 +93,49 @@ logEntryParser = do
 logParser :: Parser Log
 logParser = many $ logEntryParser <* endOfLine
 
+-- Merging data from different logs
+-- This is the European log format:
+-- 154.41.32.99 29/06/2013 15:32:23 4 internet
+
+-- Different kind of products are numbers from 1-4
+-- in the given order
+productFromID :: Int -> Product
+productFromID n = toEnum (n-1)
+
+productToID :: Product -> Int
+productToID p = fromEnum p + 1
+
+productParser2 :: Parser Product
+productParser2 = productFromID . read . (:[]) <$> digit
+
+timeParser2 :: Parser DT.LocalTime
+timeParser2 = do
+  d <- count 2 digit
+  _ <- char '/'
+  mm <- count 2 digit
+  _ <- char '/'
+  y <- count 4 digit
+  _ <- char ' '
+  h <- count 2 digit
+  _ <- char ':'
+  m <- count 2 digit
+  _ <- char ':'
+  s <- count 2 digit
+  pure $
+    DT.LocalTime { DT.localDay = DT.fromGregorian (read y) (read mm) (read d)
+                 , DT.localTimeOfDay = DT.TimeOfDay (read h) (read m) (read s) }
+
+euParser :: Parser LogEntry
+euParser = do
+  ip <- ipParser
+  _ <- char ' '
+  t <- timeParser2
+  _ <- char ' '
+  p <- productParser2
+  _ <- char ' '
+  s <- sourceParser
+  pure $ LogEntry t ip p s
+
 main :: IO ()
 main = hspec spec
 
@@ -126,3 +169,20 @@ spec = do
       parsedData `shouldBe` Right Internet
       let parsedData' = parseOnly sourceParser "noanswer"
       parsedData' `shouldBe` Right NoAnswer
+
+  describe "Parsing European data formats" $ do
+    it "leverages enums for Products" $ do
+      productFromID 1 `shouldBe` Mouse
+      productToID Keyboard `shouldBe` 2
+    it "can parse number strings into Product" $ do
+      let result = parseOnly productParser2 "4"
+      result `shouldBe` Right Speakers
+    it "can parse European date/time format" $ do
+      let sampleDateTime = "29/06/2013 15:32:23"
+          (Right result) = parseOnly timeParser2 sampleDateTime
+      DT.localDay result `shouldBe` DT.fromGregorian 2013 6 29
+    it "can parse a European logentry" $ do
+      let sampleLog = "154.41.32.99 29/06/2013 15:32:23 4 internet"
+          (Right result) = parseOnly euParser sampleLog
+      entryProduct result `shouldBe` Speakers
+
