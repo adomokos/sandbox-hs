f267f244b4adbc88d5708da3dbdf5ac7e069a2fd
Attila Domokos <adomokos@gmail.com>
2019-03-11 08:24:16 -0500

    Some metaprograming

---
+++ test/Records/MetaprogrammingSpec.hs
@@ -0,0 +1,23 @@
+{-# LANGUAGE DeriveDataTypeable #-}
+module Records.MetaprogrammingSpec where
+
+import Test.Hspec
+import Data.Data
+
+data Red = Rec {
+  alpha :: Int,
+  beta :: Double,
+  phi :: Float
+  } deriving (Data, Typeable)
+
+sample = Rec 1 2.3 4.5
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+  describe "Metaprogramming on records" $ do
+    it "works like this:" $ do
+      let fields = constrFields . toConstr $ sample
+      fields `shouldBe` ["alpha","beta","phi"]
