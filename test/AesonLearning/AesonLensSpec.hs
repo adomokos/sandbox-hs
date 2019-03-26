b53be960acb6be7b97ada65591b61512b0d70ec4
Attila Domokos <adomokos@gmail.com>
2019-03-26 11:14:55 -0500

    Compare section of JSON

--- test/AesonLearning/AesonLensSpec.hs
+++ test/AesonLearning/AesonLensSpec.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
 module AesonLearning.AesonLensSpec where
 
 import Test.Hspec
@@ -6,6 +8,9 @@ import Control.Lens
 import qualified Data.Aeson as A
 import Data.Aeson.Lens
 import qualified Data.ByteString.Lazy as B
+import GHC.Exts (fromList)
+import GHC.Generics (Generic)
+import Data.Maybe (fromJust)
 
 main :: IO ()
 main = hspec spec
@@ -16,14 +21,36 @@ jsonFile = "test/assets/employee.json"
 getJSON :: IO B.ByteString
 getJSON = B.readFile jsonFile
 
+expectedFirstDepartment :: A.Value
+expectedFirstDepartment =
+  A.Object $ fromList [
+  ("name", A.String "Help Desk"),
+  ("active", A.Bool False),
+  ("code", A.String "D38-214-89")]
+
+data Department = Department {
+    name :: !String
+  , code :: !String
+  , active :: !Bool
+  } deriving (Show, Eq, Generic)
+
+instance A.FromJSON Department
+
 spec :: Spec
 spec = do
   describe "Aeson Lens" $ do
     json <- runIO $ getJSON
     it "can read fields" $ do
-      let firstDepartment =
-            json ^? key "data" . key "person" . key "departments" . nth 0 .
-            key "name"
-      firstDepartment `shouldBe` Just "Human Resources"
       let age = json ^? key "data" . key "person" . key "age"
       age `shouldBe` Just (A.Number 28)
+      let firstDepartmentName =
+            json ^? key "data" . key "person" . key "departments" . nth 0 .
+            key "name"
+      firstDepartmentName `shouldBe` Just "Human Resources"
+
+    it "can return entire JSON Segments" $ do
+      let firstDepartment =
+            json ^? key "data" . key "person" . key "departments" . nth 1
+          expectedDept = (A.fromJSON expectedFirstDepartment :: A.Result Department)
+          dept = (A.fromJSON (fromJust firstDepartment) :: A.Result Department)
+      expectedDept `shouldBe` dept
