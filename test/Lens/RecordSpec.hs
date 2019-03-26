a8d72afbf67641c1a73d5f55d98fb026394a55e6
Attila Domokos <adomokos@gmail.com>
2019-03-26 16:57:49 -0500

    Use nested records in Lens demo

--- test/Lens/RecordSpec.hs
+++ test/Lens/RecordSpec.hs
@@ -9,8 +9,15 @@ import Test.Hspec
 import Control.Lens
 import Prelude hiding (log)
 
+data LogVar = LogVar {
+    logVarName :: !String
+  , logVarPath :: !String
+} deriving (Show, Eq)
+
+makeLensesWith camelCaseFields ''LogVar
+
 data Env = Env {
-    envLog :: !String
+    envLog :: !LogVar
   , envBalance :: !Int
   } deriving (Show, Eq)
 
@@ -19,14 +26,18 @@ makeLensesWith camelCaseFields ''Env
 main :: IO ()
 main = hspec spec
 
+logVar :: LogVar
+logVar = LogVar "system" "/etc/log"
+
 spec :: SpecWith()
 spec =
   describe "Lens for Records" $
     it "works with regular fields" $ do
-      let env = Env "the log" 100
-      env ^. log `shouldBe` "the log"
+      let env = Env logVar 100
+      env ^. log `shouldBe` logVar
+      env ^. (log . name) `shouldBe` "system"
       env ^. balance `shouldBe` 100
       let res = (balance .~ 110) env
-      res `shouldBe` Env "the log" 110
+      res `shouldBe` Env logVar 110
       let res' = set balance 111 env
-      res' `shouldBe` Env "the log" 111
+      res' `shouldBe` Env logVar 111
