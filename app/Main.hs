ba142021b05c2b914abc80122f061bd4ad54cdc2
Attila Domokos <adomokos@gmail.com>
2019-05-04 11:04:29 -0500

    Fix warning

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,8 @@
 module Main where
 
 import qualified Async.WarmUp as AW
-import qualified Errors.Runner as ER
-import qualified XmlLearning.Runner as XL
+-- import qualified Errors.Runner as ER
+-- import qualified XmlLearning.Runner as XL
 
 main :: IO ()
 main = AW.run5
